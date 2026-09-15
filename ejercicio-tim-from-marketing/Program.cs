36d91f074c84883c22704347676f5d0320f0430a
ASabinoAcosta <anakarlamarysabinoacosta@gmail.com>
2026-09-15 14:33:19 -0400

    Resolución de error: olvidé colocar el ejercicio de tim

--- ejercicio-tim-from-marketing/Program.cs
+++ ejercicio-tim-from-marketing/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿static class Badge
+{
+    public static string Print(int? id, string name, string? department)
+    {
+        string result = "";
+
+        if (id != null)
+        {
+            result += $"[{id}] - ";
+        }
+
+        result += name + " - ";
+
+        if (department == null)
+        {
+            result += "OWNER";
+        }
+        else
+        {
+            result += department.ToUpper();
+        }
+
+        return result;
+    }
+}
